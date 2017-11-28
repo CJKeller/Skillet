bdace8fe5fca91fc4b35625458a2121e3e394501
Max Kucera <m64kucera@aol.com>
2017-11-27 21:46:31 -0800

    work

---
+++ Skillet/MatchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MatchTableViewCell.swift
+//  Skillet
+//
+//  Created by Max Kucera on 11/26/17.
+//  Copyright © 2017 4CookingDummies. All rights reserved.
+//
+
+import UIKit
+
+class MatchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Skillet/VoteTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  VoteTableViewCell.swift
+//  Skillet
+//
+//  Created by Max Kucera on 11/27/17.
+//  Copyright © 2017 4CookingDummies. All rights reserved.
+//
+
+import UIKit
+
+class VoteTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var names: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
